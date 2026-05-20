77ca1b3c377d2d629f10f2e3d2692b0d87c5f644
SchemeCheese <CT060105@actvn.edu.vn>
2026-05-20 21:11:03 +0700

    feat: update next.config.ts for standalone output and webpack memory optimizations

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
+  productionBrowserSourceMaps: false,
+  experimental: {
+    webpackMemoryOptimizations: true,
+  },
   images: {
     remotePatterns: [
       {
